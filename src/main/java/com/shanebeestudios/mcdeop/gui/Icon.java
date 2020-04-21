53e917c9830c82eebfaa1ec4d85a3575fce2b510
ShaneBeee <shanebolenback@me.com>
2020-04-20 22:27:14 -0700

    Create icons

---
+++ src/main/java/com/shanebeestudios/mcdeop/gui/Icon.java
@@ -0,0 +1,35 @@
+package com.shanebeestudios.mcdeop.gui;
+
+import com.shanebeestudios.mcdeop.Main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Icon {
+
+    public static final ImageIcon DOCK_LOGO = getScaledIcon("images/1024.png", "main logo", 1024, 1024);
+
+    private static ImageIcon createImageIcon(String path, String description) {
+        java.net.URL imgURL = Main.class.getClassLoader().getResource(path);
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+    private static ImageIcon getScaledIcon(String path, String description, int w, int h) {
+        java.net.URL imgURL = Main.class.getClassLoader().getResource(path);
+        if (imgURL != null) {
+            ImageIcon i = new ImageIcon(imgURL, description);
+            Image image = i.getImage();
+            Image newImage = image.getScaledInstance(w, h, Image.SCALE_SMOOTH);
+            return new ImageIcon(newImage);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+    }
+
+}
